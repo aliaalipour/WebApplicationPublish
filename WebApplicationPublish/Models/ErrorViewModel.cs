5486c4e254306d1f1cd6071d252eca3707e729e7
ali aalipour <ali.aalipour.72@hotmail.com>
2020-10-26 21:12:13 +0330

    First Init

---
+++ WebApplicationPublish/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationPublish.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
